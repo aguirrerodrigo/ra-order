36676da04c0750616149f7a6c67ac36d1e8575cc
aguirrerodrigo <raguirre.dev@gmail.com>
2020-06-24 20:28:12 +0800

    Removed environment.ts

--- environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-	production: false,
-	project: {
-		databaseURL: 'https://ra-pos-ph.firebaseio.com',
-		projectId: 'ra-pos-ph',
-		storageBucket: 'ra-pos-ph.appspot.com'
-	}
-};
